d34ce90457398e6d7d95eba1be823bc182ab3712
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-01-24 08:36:18 +0100

    playing with React elements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+//allows to use Jsx - so to write HTML as Javascript statements
+//it will compile elements
+import React from "react";
 //React Document option model allows to import/inserts elements
 //created by React inside of the HTML elements
 import ReactDOM from "react-dom";
 
-ReactDOM.render("Hello world", document.getElementById("root"));
+const element = <h1> Hello world</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
