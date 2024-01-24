77e6967440291239e074bf5e3b102895c0628c3f
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-01-24 08:32:42 +0100

    created basic index.js file

---
+++ src/index.js
@@ -0,0 +1,5 @@
+//React Document option model allows to import/inserts elements
+//created by React inside of the HTML elements
+import ReactDOM from "react-dom";
+
+ReactDOM.render("Hello world", document.getElementById("root"));
