aca4ee57b8174b968f17721b086f31ab684b2110
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-01-24 08:43:38 +0100

    function components

--- src/index.js
+++ src/index.js
@@ -5,6 +5,35 @@ import React from "react";
 //created by React inside of the HTML elements
 import ReactDOM from "react-dom";
 
-const element = <h1> Hello world</h1>;
+//function components are more common, easier to write and maintain
 
-ReactDOM.render(element, document.getElementById("root"));
+function Webpage() {
+	const name = "David Smith";
+
+	return (
+		<>
+			<section>
+				<header>
+					<h1>Hello {name}</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+						culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</header>
+			</section>
+			<Clock />
+		</>
+	);
+}
+
+//nesting components
+function Clock() {
+	return <p> It's currently: {new Date().toLocaleTimeString("UK")} </p>;
+}
+
+ReactDOM.render(<Webpage />, document.getElementById("root"));
